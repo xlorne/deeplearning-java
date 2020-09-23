a401a53b7a1e0c95322e28efc672b262b161ce68
wangliang <1991wangliang@gmail.com>
2020-09-23 21:48:56 +0800

    SigmoidActivation SameDiff

--- demo10/src/main/java/com/codingapi/deeplearning/demo10/learn/activation/SigmoidActivation.java
+++ demo10/src/main/java/com/codingapi/deeplearning/demo10/learn/activation/SigmoidActivation.java
@@ -1,25 +1,45 @@
 package com.codingapi.deeplearning.demo10.learn.activation;
 
 import lombok.extern.slf4j.Slf4j;
+import org.nd4j.autodiff.samediff.SDVariable;
+import org.nd4j.autodiff.samediff.SameDiff;
 import org.nd4j.linalg.api.ndarray.INDArray;
-import org.nd4j.linalg.ops.transforms.Transforms;
+import org.nd4j.shade.protobuf.common.collect.Maps;
 
 /**
  * @author lorne
  * @date 2019-11-15
- * @description {1 \over {1+e^{-\W \times x}}}
+ * Sigmoid: f(x) = {1 \over {1+e^{-x}}}
  */
 @Slf4j
 public class SigmoidActivation implements Activation {
 
+
+    private transient SameDiff sameDiff;
+
+    private transient SDVariable inputs;
+
+    public SigmoidActivation() {
+        //通过自动微分 SameDiff 计算
+        sameDiff = SameDiff.create();
+        inputs =  sameDiff.var("inputs");
+
+        SDVariable exp = sameDiff.math.exp(inputs.mul(-1)).add(1);
+        sameDiff.math.pow("outputs",exp,-1);
+    }
+
     @Override
     public INDArray activation(INDArray data) {
-        return Transforms.sigmoid(data);
+        inputs.setArray(data);
+        INDArray sameDiffOut = sameDiff.output(Maps.newHashMap(),"outputs").get("outputs");
+        return sameDiffOut;
     }
 
     @Override
     public INDArray derivative(INDArray a) {
-        return a.mul(a.rsub(1));
+        sameDiff.execBackwards(null);
+        INDArray gradient =  sameDiff.getGradForVariable("inputs").getArr();
+        return gradient;
     }
 
 }
